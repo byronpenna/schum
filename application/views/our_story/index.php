8fa3543ac8faf8b53e08804f23d7f59afa85c923
JRjunior32 <junior.rod78@gmail.com>
2014-12-19 08:05:51 -0600

    our story 2

--- application/views/our_story/index.php
+++ application/views/our_story/index.php
@@ -12,13 +12,38 @@
     		$this->load->view("parts/js/script.php");
     	?>
 	</script>
+	<style type="text/css">
+		<?php
+			$this->load->view("our_story/css/style.php");
+		?>
+	</style>
 </head>
 <body>
 	<div class="container-fluid">
 		<?php $this->load->view("parts/header.php") ?>
-		<div class="row container-fluid sliderNew" >
+		<div class="row container-fluid sliderNew size" >
 			<?php $this->load->view("parts/menu.php") ?>
 		</div>
+			<div class="row container-fluid ourStoryDescription marginn">
+					<h2>Our Story</h2>
+					<p>
+						There Are Many Fantastic Real Estate Brokerages In Our Great City! We Work Diligently To Continue To Remain One Of Them.
+					</p>
+					<p>
+						Our Team Culture Is Rooted In Non-Negotiable Integrity With Laser-Focused Purpose:
+					</p>
+					<p>
+						To Serve Our Customers With Utmost Professionalism And Quality, 
+						Helping Them Achieve Their Dreams And Goals, And Prosper Along the Way. 
+						We Love Our Unique Brand and Believe Marketing Should Be Creative, Engaging And Actionable, 
+						Generating Extraordinary Results For Our Customers. 
+						We Are Passionate About Giving Back To Our Community, About Being Instrumental In Adding Quality of Life,
+						And Partnering In Making Change Happen For Those Less Fortunate In Our City.
+					</p>
+					<p>
+						Thank You For Your Trust and Business
+					</p>
+				</div>
 		<div class="bodyContainer">
 			
 		</div>
