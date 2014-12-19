bc8ac7d49856c8a2332d1165daa0310a7bd9d1bb
JRjunior32 <junior.rod78@gmail.com>
2014-12-19 08:04:45 -0600

    Our story

---
+++ application/views/our_story/css/style.php
@@ -0,0 +1,15 @@
+.size{
+	height: 225%;
+}
+.ourStoryDescription > p{
+	font-family: 'proximaRegular';
+	color: #222222;
+	text-align: justify;
+	margin-top: 2%;
+}
+.ourStoryDescription > h2{
+	color: #be0000;
+}
+.marginn{
+	margin-left: 3%;
+}
