788123b74cd9e1282a43bbe52b039fa6119cc827
JRjunior32 <junior.rod78@gmail.com>
2014-12-19 10:26:24 -0600

    algo hice no me acuerdo que xD

--- application/views/news/css/style.php
+++ application/views/news/css/style.php
@@ -6,3 +6,18 @@
 	width: 50%;
 	text-align: center;
 }
+.ourStoryDescription > p{
+	font-family: 'proximaRegular';
+	color: #222222;
+	text-align: justify;
+	margin-top: 2%;
+}
+.ourStoryDescription > h2{
+	color: #be0000;
+}
+.marginn{
+	margin-left: 3%;
+}
+.font > p{
+	font-family: 'proximaRegular';
+}
