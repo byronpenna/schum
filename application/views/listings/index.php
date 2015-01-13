97722acc5625e99a9fc3333d3fa3665eb9f975e8
kevin6911 <kevinj_1254@hotmail.com>
2015-01-13 16:20:11 -0600

    listings
    liatings

--- application/views/listings/index.php
+++ application/views/listings/index.php
@@ -35,13 +35,23 @@
     	<?php 
     		$this->load->view("listings/js/script.php");
     	?>
+    	<style type="text/css" media="screen">
+    		.amostrar{
+			width:100%;
+			margin-left:0px;
+			z-index:900000000;
+			opacity:1;
+		}
+    	</style>
 </head>
 <body>
 	<div class="container-fluid">
 		<?php $this->load->view("parts/header.php") ?>
-		<?php $this->load->view("parts/menushow.php") ?>
+		
+
 		<div class="row container-fluid sliderNew">
-			<?php $this->load->view("parts/menu.php") ?>
+		<?php /*$this->load->view("parts/menu.php") */?>	
+		<?php $this->load->view("parts/menushow.php") ?>
 		</div>
 		<div class="bodySection">
 			<!-- Property search --> 
