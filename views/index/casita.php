eccd785a39b88b151a71355a27a46bd5f5d2a447
kevin6911 <kevinj_1254@hotmail.com>
2015-02-25 17:57:44 -0600

    arreglos en our team
    our team

--- views/index/casita.php
+++
@@ -1,7 +0,0 @@
-\
-<a href='@url'>\
-	<div class='popUpMap'>\
-		<b class='calleBig'>Address:</b> <p class='calleSmall'>@calle</p> <br>\
-		<b class='calleBig'>Price:</b> <p class='calleSmall'>@price</p> <br>\
-	</div>\
-</a>
